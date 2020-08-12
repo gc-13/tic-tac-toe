c8ff4586e61f229169813c8799c79be654740310
Gabrielle Ceresa <gmc9st@virginia.edu>
2020-08-12 14:12:00 -0400

    Separated the Test cases out into different classes

--- game_test.py
+++ game_test.py
@@ -1,7 +1,7 @@
 import unittest
 import TicTacToe
 
-class MyTestCase(unittest.TestCase):
+class pVpTestCase(unittest.TestCase):
     # def test_something(self):
     #     self.assertEqual(True, False)
 
@@ -10,12 +10,6 @@ class MyTestCase(unittest.TestCase):
         player1 = TicTacToe.Player('X', game1)
         player2 = TicTacToe.Player('O', game1)
         return (game1, player1, player2)
-
-    def onecompplayer_setup(self):
-        game1 = TicTacToe.Game()
-        computer1 = TicTacToe.Computer('X', game1)
-        player2 = TicTacToe.Player('O', game1)
-        return (game1, computer1, player2)
 
     #Player 1 wins
     def test_mock_game1(self):
@@ -41,18 +35,54 @@ class MyTestCase(unittest.TestCase):
         winner = game1.play_test(player1, player2, p1moves, p2moves)
         self.assertEqual(None, winner)
 
+class CvPTestCase(unittest.TestCase):
+
+    def onecompplayer_setup(self):
+        game1 = TicTacToe.Game()
+        computer1 = TicTacToe.Computer('X', game1)
+        player2 = TicTacToe.Player('O', game1)
+        return (game1, computer1, player2)
+
     def test_place3(self):
         game1, computer1, player2 = self.onecompplayer_setup()
         game1.game_board = {1:"X", 2:"X",3:"-",
                            4:"-", 5:"-", 6:"-",
                            7:"-", 8:"-", 9:"-"}
-        p2moves = ["middle", "bottom left"]
+        p2moves = []
         winner = game1.play_comp_test(computer1, player2, p2moves)
         self.assertEqual(computer1, winner)
         self.assertEqual({1:"X", 2:"X",3:"X",
                            4:"-", 5:"-", 6:"-",
                            7:"-", 8:"-", 9:"-"},
                         game1.game_board
+                         )
+
+    def test_place2(self):
+        game1, computer1, player2 = self.onecompplayer_setup()
+        game1.game_board = {1:"-", 2:"-",3:"-",
+                           4:"-", 5:"X", 6:"-",
+                           7:"-", 8:"X", 9:"-"}
+        p2moves = []
+        winner = game1.play_comp_test(computer1, player2, p2moves)
+        self.assertEqual(computer1, winner)
+        self.assertEqual({1:"-", 2:"X",3:"-",
+                           4:"-", 5:"X", 6:"-",
+                           7:"-", 8:"X", 9:"-"},
+                        game1.game_board
+                         )
+
+    def test_place8(self):
+        game1, computer1, player2 = self.onecompplayer_setup()
+        game1.game_board = {1: "-", 2: "-", 3: "-",
+                            4: "-", 5: "-", 6: "-",
+                            7: "X", 8: "-", 9: "X"}
+        p2moves = []
+        winner = game1.play_comp_test(computer1, player2, p2moves)
+        self.assertEqual(computer1, winner)
+        self.assertEqual({1: "-", 2: "-", 3: "-",
+                          4: "-", 5: "-", 6: "-",
+                          7: "X", 8: "X", 9: "X"},
+                         game1.game_board
                          )
 
 if __name__ == '__main__':
